80a051d21b6b383961e5ccbdc447b2138becd245
Eric <chenghaowang03@gmail.com>
2021-02-08 17:22:31 +0800

    bla

--- a1_test.py
+++ a1_test.py
@@ -2,8 +2,6 @@ import re
 import html
 import spacy
 
-spacy.cli.download("en_core_web_sm")
-
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 sentencizer = nlp.create_pipe("sentencizer")
 nlp.add_pipe(sentencizer)
